0c9479749a55d52829bb775393028b92a32dc051
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-05-30 15:25:43 +0900

    add

--- app_home.py
+++ app_home.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 def run_home():
     st.subheader('왼쪽 사이드바에서 원하는 내용을 선택하세요.')
-    st.text('이 앱은 간단한 기상정보를 사용하여 교통사고율을 예상할 수 있는 앱입니다.')
+    st.text('이 앱은 생활패턴 정보를 사용하여 비만정도를 예상할 수 있는 앱입니다.')
     st.subheader("")
-    st.info("사용한 데이터:\n1. [종관기상] 지점별 연·월 통계\n2. 시・도별 도로보급률\n3. 시도별 교통사고\n4. 도로교통사고 사망률")
+    st.info("사용한 데이터:\n1. 비만 유병률(체질량지수 기준) 추이\n2. 연령별 성별 대사증후군 위험요인 보유개수별 현황\n3. 연령별 성별 대사증후군 위험요인별 현황\n4. 가공식품 구입 주기\n5. (지난 1년 동안) 휴가 경험 여부\n6. 스마트 기기 활용 시간_평일\n7. Obesity Levels & Life Style\n\n데이터분석에는 한국 통계를, 머신러닝에는 남미 통계를 사용했습니다.")
     st.text('')
     st.text('')
     st.text('')
@@ -17,5 +17,5 @@ def run_home():
     st.text('')
 
 
-    st.text('출처:\nhttps://kosis.kr/statHtml/statHtml.do?orgId=132&tblId=DT_V_MOTA_016\nhttps://www.index.go.kr/unify/idx-info.do?idxCd=4261')
+    st.text('출처:\nhttps://kosis.kr/statHtml/statHtml.do?orgId=177&tblId=DT_11702_N101&conn_path=I2')
 
