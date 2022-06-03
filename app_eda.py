284994dddde15d605c974a2d55069f2357448834
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-06-03 16:56:16 +0900

    add

--- app_eda.py
+++ app_eda.py
@@ -78,8 +78,8 @@ def run_eda():
             data_topic = ['전체','비만유병률 ','대사증후군 위험요인','대사증후군 발병현황','가공식품 섭취빈도','휴가 사용유무','스마트기기 사용시간']
             selected_data = st.selectbox('보고싶은 데이터프레임을 고르세요.',data_topic)
 
-            age_list = ['전체','20대','30대','40대','50대','60대']
-            get_age = st.selectbox('해당 연도의 데이터를 검색합니다.',age_list) 
+            age_list = ['전체 나이대','20대','30대','40대','50대','60대']
+            get_age = st.selectbox('해당 나이대의 데이터를 검색합니다.',age_list) 
             if get_age == age_list[0]:
                 get_age = '|'.join(age_list[1:])
             age_txt = get_age     
@@ -97,12 +97,12 @@ def run_eda():
 
             #전체
             if selected_data == data_topic[0]:
-                st.dataframe(df.sort_values(['년도','나이대'],axis=0))
+                st.dataframe(df.loc[df['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0))
 
             #비만유병률
             elif selected_data == data_topic[1]:
                 
-                st.dataframe(df.iloc[:,:3].sort_values(['년도','나이대'],axis=0))
+                st.dataframe(df.iloc[:,:3].loc[df['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0))
                 st.text('*비만유병률은 체질량지수(bmi)가 25kg/㎡ 이상인 사람들의 분율입니다.')
                 if st.button('차트 생성'):
                     st.info('년도별, 나이대별로 비만유병률을 나타낸 차트입니다.')
@@ -111,18 +111,20 @@ def run_eda():
 
             #대사증후군 위험요인 (개별파일 필요)
             elif selected_data == data_topic[2]:
-                st.dataframe(df_disease.sort_values(['년도','나이대'],axis=0))
+                df_disease_sel = df_disease.loc[df_disease['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0)
+                st.dataframe(df_disease_sel)
                 if st.button('차트 생성'):
                     st.info('대사증후군을 앓고있는 사람중 관련 위험요인을 보유한 사람의 분율을 나타낸 차트입니다.')
-                    fig = px.pie(df_disease, values='명', names='유형',hole=.3)
+                    fig = px.pie(df_disease_sel, values='명', names='유형',hole=.3)
                     st.plotly_chart(fig)
 
             #대사증후군 발병현황 (개별파일 필요)
             elif selected_data == data_topic[3]:
-                st.dataframe(df_meta.sort_values(['년도','나이대'],axis=0))
+                df_meta_sel = df_meta.loc[df_meta['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0)
+                st.dataframe(df_meta_sel)
                 if st.button('차트 생성'):
                     st.info('대사증후군 발병현황을 나타낸 차트입니다.')
-                    meta_vl = year_separater(df_meta,'년도','명','유형')
+                    meta_vl = year_separater(df_meta_sel,'년도','명','유형')
                     fig = make_subplots(1, 3, specs=[[{'type':'domain'}, {'type':'domain'},{'type':'domain'}]],
                     subplot_titles=['2018', '2019','2020'])
                     fig.add_trace(go.Pie(values = meta_vl[0], labels = meta_vl[1], hole = 0.3,pull=[0.1,0,0]),1,1)
@@ -133,10 +135,11 @@ def run_eda():
             
             #가공식품 소비빈도 (개별파일 필요)
             elif selected_data == data_topic[4]:  # 설문형 결과
-                st.dataframe(df_processed_food.sort_values(['년도','나이대'],axis=0))
+                df_processed_food_sel = df_processed_food.loc[df_processed_food['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0)
+                st.dataframe(df_processed_food_sel)
                 if st.button('차트 생성'):
                     st.info('가공식품 소비빈도를 나타낸 차트입니다.')
-                    pf_vl = year_separater(df_processed_food,'년도','분율','유형')
+                    pf_vl = year_separater(df_processed_food_sel,'년도','분율','유형')
                     fig = make_subplots(1, 3, specs=[[{'type':'domain'}, {'type':'domain'},{'type':'domain'}]],
                     subplot_titles=['2018', '2019','2020'])
                     fig.add_trace(go.Pie(values = pf_vl[0], labels = pf_vl[1], hole = 0.3,pull=[0,0.1,0,0,0,0]),1,1)
@@ -146,10 +149,11 @@ def run_eda():
             
             #휴가 사용유무 (개별파일 필요)
             elif selected_data == data_topic[5]:
-                st.dataframe(df_vacation.sort_values(['년도','나이대'],axis=0))
+                df_vacation_sel = df_vacation.loc[df_vacation['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0)
+                st.dataframe(df_vacation_sel)
                 if st.button('차트 생성'):
                     st.info('휴가 사용유무를 나타낸 차트입니다.')
-                    pf_vc = year_separater(df_vacation,'년도','분율','유형')
+                    pf_vc = year_separater(df_vacation_sel,'년도','분율','유형')
                     fig = make_subplots(1, 3, specs=[[{'type':'domain'}, {'type':'domain'},{'type':'domain'}]],
                     subplot_titles=['2018', '2019','2020'])
                     fig.add_trace(go.Pie(values = pf_vc[0], labels = pf_vc[1], hole = 0.3,pull=[0,0.05]),1,1)
@@ -159,15 +163,16 @@ def run_eda():
             
             #스마트기기 사용시간  (개별파일 필요) # 설문형 결과
             elif selected_data == data_topic[6]:  
-                st.dataframe(df_smart.sort_values(['년도','나이대'],axis=0))
+                df_smart_sel = df_smart.loc[df_smart['나이대'].str.contains(get_age),].sort_values(['년도','나이대'],axis=0)
+                st.dataframe(df_smart_sel)
                 if st.button('차트 생성'):
                     st.info('스마트기기 사용시간을 나타낸 차트입니다.')
-                    pf_sm = year_separater(df_smart,'년도','분율','유형')
+                    pf_sm = year_separater(df_smart_sel,'년도','분율','유형')
                     fig = make_subplots(1, 3, specs=[[{'type':'domain'}, {'type':'domain'},{'type':'domain'}]],
                     subplot_titles=['2018', '2019','2020'])
-                    fig.add_trace(go.Pie(values = pf_sm[0], labels = pf_sm[1], hole = 0.3,pull=[0,0,0.05,0.05,0.05]),1,1)
-                    fig.add_trace(go.Pie(values = pf_sm[2], labels = pf_sm[3], hole = 0.3,pull=[0,0,0.05,0.05,0.05]),1,2)
-                    fig.add_trace(go.Pie(values = pf_sm[4], labels = pf_sm[5], hole = 0.3,pull=[0,0,0.05,0.05,0.05]),1,3)
+                    fig.add_trace(go.Pie(values = pf_sm[0], labels = pf_sm[1], hole = 0.3,pull=[0,0,0.1,0.1,0.1]),1,1)
+                    fig.add_trace(go.Pie(values = pf_sm[2], labels = pf_sm[3], hole = 0.3,pull=[0,0,0.1,0.1,0.1]),1,2)
+                    fig.add_trace(go.Pie(values = pf_sm[4], labels = pf_sm[5], hole = 0.3,pull=[0,0,0.1,0.1,0.1]),1,3)
                     st.plotly_chart(fig)
 
 
